8bc95dc53e81ee98a64f71ee5c3cb3591f3b099b
Cesare <100310104+Kynesy@users.noreply.github.com>
2023-11-05 16:32:11 +0100

    spring boot initialize

---
+++ src/main/java/it/unisalento/pas/loginbe/loginbe/LoginBeApplication.java
@@ -0,0 +1,13 @@
+package it.unisalento.pas.loginbe.loginbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginBeApplication.class, args);
+    }
+
+}
