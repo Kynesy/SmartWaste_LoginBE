367d09400a8bbe513468ed9dae300e143d0900fc
Cesare <100310104+Kynesy@users.noreply.github.com>
2023-11-05 17:02:28 +0100

    bug fix

--- src/main/java/it/unisalento/pas/loginbe/LoginBeApplication.java
+++ src/main/java/it/unisalento/pas/loginbe/LoginBeApplication.java
@@ -1,4 +1,4 @@
-package it.unisalento.pas.loginbe.loginbe;
+package it.unisalento.pas.loginbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LoginBeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(LoginBeApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(LoginBeApplication.class, args);
+	}
 
 }
---
+++ src/test/java/it/unisalento/pas/loginbe/LoginBeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unisalento.pas.loginbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
