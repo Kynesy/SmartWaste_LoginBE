c40a59f87a2f02dd3da6437e4939ad3c502fad7d
Cesare <100310104+Kynesy@users.noreply.github.com>
2023-11-09 13:09:03 +0100

    Update UserController.java

--- src/main/java/it/unisalento/pas/loginbe/controllers/UserController.java
+++ src/main/java/it/unisalento/pas/loginbe/controllers/UserController.java
@@ -2,7 +2,9 @@ package it.unisalento.pas.loginbe.controllers;
 
 import it.unisalento.pas.loginbe.domain.User;
 import it.unisalento.pas.loginbe.dto.UserDTO;
+import it.unisalento.pas.loginbe.security.AuthorizedApplications;
 import it.unisalento.pas.loginbe.services.UserService;
+import it.unisalento.pas.loginbe.utils.JwtGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/user")
 public class UserController {
     private final UserService userService;
+    private final JwtGenerator jwtGenerator = new JwtGenerator();
+    private final AuthorizedApplications authorizer = new AuthorizedApplications();
 
     @Autowired
     public UserController(UserService userService) {
@@ -22,41 +26,63 @@ public class UserController {
     public ResponseEntity<String> createUser(@RequestBody UserDTO userDTO) {
         User user = fromUserDTOtoUser(userDTO);
 
-        System.out.println("USER: " + user.getApplicationId());
-        System.out.println("USER: " + user.getEmail());
-        System.out.println("USER: " + user.getPassword());
-        System.out.println("USER: " + user.getId());
+        if(authorizer.isApplicationIDnotValid(user.getApplicationId())){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Application ID not valid");
+        }
 
-        int result = userService.saveUser(user);
+        int result = userService.createUser(user);
         if (result == 0) {
-            return ResponseEntity.status(HttpStatus.OK).body("{\"message\": \"User created successfully\"}");
+            return ResponseEntity.status(HttpStatus.OK).body(jwtGenerator.generateLoginJSON(user));
         } else {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"message\": \"User creation failed\"}");
         }
     }
 
+    @GetMapping("/exist/{userID}")
+    public ResponseEntity<Boolean> existUser(@PathVariable String userID) {
+        if (userService.userExist(userID)) {
+            return ResponseEntity.status(HttpStatus.OK).body(true);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(false);
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody UserDTO userDTO){
+        User user = fromUserDTOtoUser(userDTO);
+
+        if(authorizer.isApplicationIDnotValid(user.getApplicationId())){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Application ID not valid");
+        }
+
+        int result = userService.checkCredentials(user);
+        String responseMessage;
+
+        return switch (result) {
+            case 0 -> {
+                responseMessage = jwtGenerator.generateLoginJSON(userService.getUser(user.getEmail(), user.getApplicationId()));
+                yield ResponseEntity.ok(responseMessage);
+            }
+            case 2 -> {
+                responseMessage = "Wrong credentials.";
+                yield ResponseEntity.status(HttpStatus.FORBIDDEN).body(responseMessage);
+            }
+            default -> {
+                responseMessage = "Utente non esistente o altro.";
+                yield ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseMessage);
+            }
+        };
+    }
+
     private User fromUserDTOtoUser(UserDTO userDTO) {
         User user = new User();
-
-        System.out.println("DTO: " + userDTO.getApplicationId());
-        System.out.println("DTO: " + userDTO.getEmail());
-        System.out.println("DTO: " + userDTO.getPassword());
-        System.out.println("DTO: " + userDTO.getId());
 
         user.setId(userDTO.getId());
         user.setEmail(userDTO.getEmail());
         user.setPassword(userDTO.getPassword());
         user.setApplicationId(userDTO.getApplicationId());
+        user.setRole(userDTO.getRole());
 
         return user;
-    }
-
-    @GetMapping("/exist/{userID}")
-    public ResponseEntity<Boolean> existUser(@PathVariable String userID) {
-        if (userService.userExist(userID) == 1) {
-            return ResponseEntity.status(HttpStatus.OK).body(true);
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(false);
-        }
     }
 }
