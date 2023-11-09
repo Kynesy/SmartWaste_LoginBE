325a63b1abe3c8a9595aeb10c09f30923a423857
Cesare <100310104+Kynesy@users.noreply.github.com>
2023-11-09 13:08:42 +0100

    introdotto il controllo incrociato con l'application id

--- src/main/java/it/unisalento/pas/loginbe/repository/IUserRepository.java
+++ src/main/java/it/unisalento/pas/loginbe/repository/IUserRepository.java
@@ -4,4 +4,6 @@ import it.unisalento.pas.loginbe.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface IUserRepository extends MongoRepository<User, String> {
+    User findByEmailAndApplicationId(String email, String applicationId);
+    boolean existsByEmailAndApplicationId(String email, String applicationId);
 }
