27445238980a1adbfe5061bcdf4f90450e2ece3a
Cesare <100310104+Kynesy@users.noreply.github.com>
2023-11-26 19:23:43 +0100

    creazione service per user details

---
+++ src/main/java/it/unisalento/pas/loginbe/services/UserDetailsCustomService.java
@@ -0,0 +1,28 @@
+package it.unisalento.pas.loginbe.services;
+
+import it.unisalento.pas.loginbe.models.User;
+import it.unisalento.pas.loginbe.models.UserDetailsCustom;
+import it.unisalento.pas.loginbe.respositories.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+public class UserDetailsCustomService implements UserDetailsService {
+    @Autowired
+    IUserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsCustom.build(user);
+    }
+
+}
