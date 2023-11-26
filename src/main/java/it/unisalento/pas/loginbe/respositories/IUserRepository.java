7be039ed6548b70bd76b37326fe4176788d0f1b7
Cesare <100310104+Kynesy@users.noreply.github.com>
2023-11-26 19:24:35 +0100

    creazione classe user repository per mongo db

---
+++ src/main/java/it/unisalento/pas/loginbe/respositories/IUserRepository.java
@@ -0,0 +1,12 @@
+package it.unisalento.pas.loginbe.respositories;
+
+import it.unisalento.pas.loginbe.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.Optional;
+
+public interface IUserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+}
+
