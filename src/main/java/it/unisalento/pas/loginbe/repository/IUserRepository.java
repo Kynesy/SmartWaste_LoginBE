5cebae08e3bd642deeae5e7fe5a56c72538b14b2
Cesare <100310104+Kynesy@users.noreply.github.com>
2023-11-26 19:23:11 +0100

    manutenzione codice

--- src/main/java/it/unisalento/pas/loginbe/repository/IUserRepository.java
+++
@@ -1,9 +0,0 @@
-package it.unisalento.pas.loginbe.repository;
-
-import it.unisalento.pas.loginbe.domain.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface IUserRepository extends MongoRepository<User, String> {
-    User findByEmailAndApplicationId(String email, String applicationId);
-    boolean existsByEmailAndApplicationId(String email, String applicationId);
-}
--- src/main/java/it/unisalento/pas/loginbe/utils/IPasswordHasher.java
+++
@@ -1,6 +0,0 @@
-package it.unisalento.pas.loginbe.utils;
-
-public interface IPasswordHasher {
-    String hashPassword(String password);
-    boolean checkPassword(String hashedPass, String password);
-}
